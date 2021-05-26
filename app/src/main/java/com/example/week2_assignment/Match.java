ef0df3fb542a9f094cbaf6fd32ff9f8caa3e3b74
Aaron <imaaronacs@gmail.com>
2021-05-26 13:16:52 -0700

    Fixed Week 6 Assignment, pictures now loading, MVVM followed

--- app/src/main/java/com/example/week2_assignment/Match.java
+++ app/src/main/java/com/example/week2_assignment/Match.java
@@ -3,12 +3,16 @@ package com.example.week2_assignment;
 public class Match
 {
     private String name;
+    private String imageUrl;
+    private boolean liked;
 
     public Match(){}
 
-    public Match(String name)
+    public Match(String name, String imageUrl, boolean liked)
     {
         this.name = name;
+        this.imageUrl = imageUrl;
+        this.liked = liked;
     }
 
     public void setName(String name)
@@ -16,8 +20,28 @@ public class Match
         this.name = name;
     }
 
+    public void setImageUrl(String imageUrl)
+    {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setLiked(boolean liked)
+    {
+        this.liked = liked;
+    }
+
     public String getName()
     {
         return name;
+    }
+
+    public String getImageUrl()
+    {
+        return imageUrl;
+    }
+
+    public boolean getLiked()
+    {
+        return liked;
     }
 }
--- app/src/main/java/com/example/week2_assignment/ui/main/MatchesViewAdapter.java
+++ app/src/main/java/com/example/week2_assignment/ui/main/MatchesViewAdapter.java
@@ -1,28 +1,25 @@
 package com.example.week2_assignment.ui.main;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.week2_assignment.Match;
 import com.example.week2_assignment.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MatchesViewAdapter extends FirebaseRecyclerAdapter<Match, MatchesViewAdapter.MatchesViewHolder>
 {
@@ -50,12 +47,19 @@ public class MatchesViewAdapter extends FirebaseRecyclerAdapter<Match, MatchesVi
     @Override
     protected void onBindViewHolder(@NonNull MatchesViewHolder holder, int position, @NonNull Match model) {
         holder.nameTV.setText(model.getName());
+        ImageView imageView = ((MatchesViewHolder) holder).profilePicture;
+        Glide.with(holder.itemView.getContext()).
+                load(model.getImageUrl()).
+                apply(new RequestOptions().override(100, 100)).
+                into(imageView);
     }
+
 
 
     public class MatchesViewHolder extends RecyclerView.ViewHolder
     {
         TextView nameTV;
+        ImageView profilePicture;
         Button b;
 
         public MatchesViewHolder(@NonNull View itemView)
@@ -63,13 +67,16 @@ public class MatchesViewAdapter extends FirebaseRecyclerAdapter<Match, MatchesVi
             super(itemView);
 
             nameTV = itemView.findViewById(R.id.card_view_name);
+            profilePicture = itemView.findViewById(R.id.profile_picture_card);
             b = itemView.findViewById(R.id.card_view_btn);
             b.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+
                     Toast.makeText(itemView.getContext(), "You have liked " + nameTV.getText().toString(), Toast.LENGTH_LONG).show();
                 }
             });
         }
+
     }
 }
